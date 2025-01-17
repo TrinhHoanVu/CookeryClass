58642f098c0b4fced54d88605024c1b153b65c70
kar1a <trinhhoanvu25041998@gmail.com>
2025-01-17 20:14:17 +0700

    contest-management 17/01/2025

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ import { DataProvider } from './context/DatabaseContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <BrowserRouter>
       <DataProvider>
         <App />
       </DataProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
